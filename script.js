0da22db4c2fd21f2c793919416fce7a24c2b9c15
kn5225 <149865521+kn5225@users.noreply.github.com>
2026-02-08 17:41:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 VerMode() {
   var input=document.createElement("input");
   input.type = "text";
-  container.appendChild(input);
-
-
+  input.id="VerInput";
+  var button=document.createElement("button");
+  button.type="button"
+  button.id="VerModeSubmit"
+  button.innerText="Submit"
+  var element2 = document.getElementbyId("MainArea");
+  element2.appendChild(input);
+  element2.appendChild(button);
+  var element3=document.getElementById("VerModeSubmit");
+  element3.addEventListener(click, VerModeAccept);
 }
