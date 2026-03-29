94fd591a824c58837c013ee051616e72240660f1
kn5225 <149865521+kn5225@users.noreply.github.com>
2026-03-29 19:30:40 -0400

    Create demo-config.js

---
+++ demo-admin/demo-config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = "https://dfzsfjcmtxvgotzpdagy.supabase.co"
+export const SUPABASE_KEY = "sb_publishable_YWYDtyLqRbevoyWsd5i_jQ_vnvqaRBf"
+export const COURSES_TABLE = "demo_prereqlookup"
+export const SUBMISSIONS_TABLE = "demo_user_submissions"
