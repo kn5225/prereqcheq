30a6b20ad1128df0bf387dd45097fb2efdd53cb5
kn5225 <149865521+kn5225@users.noreply.github.com>
2026-03-29 21:35:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 export const SUPABASE_URL = "https://dfzsfjcmtxvgotzpdagy.supabase.co"
 export const SUPABASE_KEY = "sb_publishable_YWYDtyLqRbevoyWsd5i_jQ_vnvqaRBf"
+export const COURSES_TABLE = "prereqlookup"
+export const SUBMISSIONS_TABLE = "user_submissions"
