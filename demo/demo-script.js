991f908ea7c5bf3b774a368d3ab45101405877ba
Kanav Nagpal <149865521+kn5225@users.noreply.github.com>
2026-07-09 23:15:10 -0400

    Update demo-script.js

--- demo/demo-script.js
+++ demo/demo-script.js
@@ -139,6 +139,8 @@ function VerMode() {
   let MainArea = getMainArea();
   MainArea.innerHTML = "";
   const p = makeElement("p", { type: "text", id: "VerText", textContent: "  Enter course to check:" });
+  p.style.fontWeight = "500"
+  p.style.marginBottom = "8px"
   const input = makeElement("input", { type: "text", id: "VerInput", placeholder: "Enter course: (Eg. DEMO 202)" });
   const checkBoxDiv = makeElement("div", { id: "checkBoxDiv" });
   const dropdown = makeElement("div", { id: "VerDropdown" })
